1072d1c394a1095574554a97d971b4f2f1d52858
filteredwaterdev <311787702+filteredwaterdev@users.noreply.github.com>
2026-08-02 21:11:44 +0800

    Create ExportIcon.tsx

---
+++ assets/react/ExportIcon.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface ExportIconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number
+  color?: string
+  strokeWidth?: number
+}
+
+const ExportIcon: React.FC<ExportIconProps> = ({
+  size = 24,
+  color = 'currentColor',
+  strokeWidth = 1.5,
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill="none"
+    stroke={color}
+    strokeWidth={strokeWidth}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    aria-label="Save to file"
+    role="img"
+    {...props}
+  >
+    <path d="M2 22V2h7l4 4v16H2z" />
+    <path d="M9 2v4h4" />
+    <path d="M14 12h8" />
+    <path d="M19 9l3 3-3 3" />
+  </svg>
+)
+
+export default ExportIcon
